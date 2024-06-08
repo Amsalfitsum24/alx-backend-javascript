dd9c07a5f0bfec761ee6ae9b7b51168d79cbd61f
Amsalfitsum24 <amsalfessha21@gmail.com>
2024-06-08 01:06:51 +0000

    fix node

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
@@ -8,16 +7,23 @@ const countStudents = (dataPath) => {
   if (!fs.statSync(dataPath).isFile()) {
     throw new Error('Cannot load the database');
   }
+
   const fileLines = fs
     .readFileSync(dataPath, 'utf-8')
     .toString('utf-8')
     .trim()
     .split('\n');
+
+  if (fileLines.length === 0) {
+    throw new Error('Cannot load the database');
+  }
+
   const studentGroups = {};
   const dbFieldNames = fileLines[0].split(',');
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
+    if (line.trim() === '') continue; // Skip empty lines
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
@@ -29,9 +35,9 @@ const countStudents = (dataPath) => {
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
+  const totalStudents = Object.values(studentGroups)
+    .reduce((pre, cur) => pre + cur.length, 0);
+
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
@@ -40,3 +46,4 @@ const countStudents = (dataPath) => {
 };
 
 module.exports = countStudents;
+
